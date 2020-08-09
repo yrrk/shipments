0ae9b41bc1ccea7a09801ded65c23343fd0f3604
yaksh thakkar <kinyakrr@gmail.com>
2020-08-09 13:28:08 +0530

    Updates

---
+++ precache-manifest.5598c90979c3d8342dcdbe271bd12d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9e22f58bb873d41443c897584532dc",
+    "url": "/shipments/index.html"
+  },
+  {
+    "revision": "4734fed0db14b52a9749",
+    "url": "/shipments/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c930882219e7b6690fd",
+    "url": "/shipments/static/js/2.784383fe.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/shipments/static/js/2.784383fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4734fed0db14b52a9749",
+    "url": "/shipments/static/js/main.94ba641e.chunk.js"
+  },
+  {
+    "revision": "23602d1ab2775108117b",
+    "url": "/shipments/static/js/runtime-main.f5d73850.js"
+  }
+]);
